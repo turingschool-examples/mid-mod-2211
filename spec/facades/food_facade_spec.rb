b6b7f8d7fc729d472424b17d8eae003104d1070e
Weston Sandfort <sandfortw@gmail.com>
2023-04-07 10:42:51 -0600

    create a test for food facade

---
+++ spec/facades/food_facade_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe FoodFacade do
+  let(:params) { {q: 'sweet potatoes'} }
+
+  describe '#foods' do
+    it 'returns an array of food objects' do
+      expect(FoodFacade.new(params).foods).to be_an Array
+      expect(FoodFacade.new(params).foods.first).to be_a Food
+    end
+  end
+
+  describe '#count' do
+    it 'returns an integer' do
+      expect(FoodFacade.new(params).count).to be_an Integer
+    end
+  end
+end
