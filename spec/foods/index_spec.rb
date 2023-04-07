340a4617ca32e807e623ad79fe5df31f0de30f5c
Andra Helton <andrahelton@gmail.com>
2023-04-07 09:21:24 -0600

    Add adjusted tests

---
+++ spec/foods/index_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Foods Index", type: :feature do
+  describe "As a user, When I visit '/foods'" do
+    context "I fill in the search form on '/' with 'sweet potatoes' and click 'Search'" do
+      it "I should be on page '/foods'"
+      it "I should see a total of the number of items returned by the search"
+      it "I should see a list of TEN foods that contain the ingredient 'sweet potatoes'"
+      
+      describe "for each of the foods I should see" do
+        it "food's GTIN/UPC code"
+        it "food's description"
+        it " food's Brand Owner"
+        it "food's ingredients"
+      end
+    end
+  end
+end
