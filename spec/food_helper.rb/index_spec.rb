b57ba435dc58923fb5e6dcb46dc8bbabad906abc
Weston Sandfort <sandfortw@gmail.com>
2023-04-07 09:26:34 -0600

    continue to write tests for food index page

--- spec/food_helper.rb/index_spec.rb
+++ spec/food_helper.rb/index_spec.rb
@@ -14,7 +14,13 @@ describe 'Food Index Page' do
       expect(current_path).to eq(foods_path)
     end
 
-    
+    it 'should have a total number of food items' do
+      expect(page).to have content('Total Hits: 57086')
+    end
+
+    it 'should only have 10 food items' do
+      expect(page).to have_css("Food:", count: 10)
+    end
 
   end
 end
