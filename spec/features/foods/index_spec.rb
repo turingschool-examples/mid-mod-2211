70eeb1f04a931016d244f268571916779f880886
Bobby Luly <bobbyluly@gmail.com>
2023-04-07 10:22:17 -0600

    Test: create test for the foods index page

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Food Index Page' do
+  describe 'As a visitor' do
+    describe 'When I visit the food index page' do
+      it 'I see a list of the 10 foods that contain the ingredient "sweet potatoes"' do
+        visit root_path
+
+        fill_in :q, with: 'sweet potatoes'
+        click_button 'Search'
+
+        expect(current_path).to eq(foods_path)
+        expect(page).to have_content("10 Results")
+        expect(page).to have_content("GTIN/UPC code: 728229015529")
+        expect(page).to have_content("Description: SWEETS MEDLEY SEL GRIS, BATATA, SWEET POTATO, PURPLE SWEET POTATO REAL VEGETABLE CHIPS, SWEETS MEDLEY")
+        expect(page).to have_content("Brand Owner: The Hain Celestial Group, Inc.")
+        expect(page).to have_content("Ingredients: A BLEND OF SWEET POTATOES (SWEET POTATO, PURPLE SWEET POTATO, BATATA), AVOCADO OIL, SEA SALT.")
+      end
+    end
+  end
+end
